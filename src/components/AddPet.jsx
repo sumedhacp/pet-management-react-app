27b3c411674d032caf8df931515baf622c638e1b
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-15 02:44:44 +0530

    Completed

--- src/components/AddPet.jsx
+++ src/components/AddPet.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import NavigationBar from './NavigationBar'
 
 
 const AddPet = () => {
@@ -55,7 +56,7 @@ const AddPet = () => {
 
     return (
         <div>
-
+            <NavigationBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
--- src/components/ViewPet.jsx
+++ src/components/ViewPet.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import NavigationBar from './NavigationBar'
 
 const ViewPet = () => {
 
@@ -23,7 +24,7 @@ const ViewPet = () => {
 
     return (
         <div>
-
+            <NavigationBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
